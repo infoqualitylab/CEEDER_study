34530798a4e68cce9507dceddc2b5ca9d446c3ac
unknown <christoph-stade@outlook.de>
2024-08-15 12:40:56 -0500

    Update analysis and add csv

--- Summer2024_Christoph/similarity.py
+++ Summer2024_Christoph/similarity.py
@@ -1,20 +1,13 @@
-
 import requests
 import csv
 import json
 
 import networkx as nx
-from networkx.algorithms.community import louvain_communities
 import matplotlib.pyplot as plt
-
 import hvplot.networkx as hvnx
+from networkx.algorithms.community import louvain_communities
+from wordcloud import WordCloud
 
-import networkx as nx
-import holoviews as hv
-
-
-TEST_DOI = '10.1111/geb.13597' # Meta-Analysis on Land use...
-API_KEY = '11852fb061166663a048c15071f4d873'
 
 # 1. Data retrieval
 
@@ -27,7 +20,7 @@ sources = {
 }
 
 api_keys = {
-    "scopus": '11852fb061166663a048c15071f4d873', # From Ishita's git
+    "scopus": '...', # From Ishita's git
     "open_alex": '', # It's open!
     # "web_of_science": f'' # tbd
 }
@@ -36,26 +29,33 @@ api_keys = {
 # for source in sources.values():
 #     pass
 
-review_dois = []
+# Structure: [{ "title": "abc", "doi": 123, "question": "abc?", "references": [ 1, 2, 3 ] }, { ... }]
+review_meta_mappings = []
 
 # Extract review DOIs from CEEDER csv dump
-with open('C:/Users/chris/Documents/ObsidianVault/prof/files//csv/CEEDER_OR_all.csv', "r", encoding="UTF-8") as csvfile:
+with open('./CEEDER_reviews_climate_collection', "r", encoding="UTF-8") as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
 
     for row in reader:
-        # Extract DOI from inbetween 2 HTML tags
         try:
-            review_dois.append(row.get("Link").split(">")[1].split("<")[0])
+            review_meta_mappings.append({
+                "question": row.get("Title"),
+                "doi": row.get("Link").split(">")[1].split("<")[0], # Extract DOI from inbetween 2 HTML tags
+                "question": row.get("Review Question")
+            })
         except:
             # No DOI
             # Strategy: ignore. Seems to only be a single review
             pass
 
-review_citation_list_mapping = {}
 broken_dois = []
 empty_references = []
 
-for doi in review_dois:
+# TODO: Do this more elegantly? pop() has a lot of ugly consequences, that need to be worked around...
+for review_copy in review_meta_mappings.copy(): # Iterate copy, because of pop() operation
+    i = review_meta_mappings.index(review_copy) # Make i dynamic, because of changing length of list through pop() operation
+    review = review_meta_mappings[i]
+    doi = review["doi"]
     response = requests.get(f'https://api.openalex.org/works?filter=doi:{doi}')
 
     if response.status_code == 200:
@@ -70,15 +70,17 @@ for doi in review_dois:
 
     if results == []:
         broken_dois.append(doi)
+        review_meta_mappings.pop(i) # Remove review with faulty meta
         continue 
 
     references = results[0]["referenced_works"]
 
     if references == []:
         empty_references.append(doi)
+        review_meta_mappings.pop(i) # Remove review with faulty meta
         continue
 
-    review_citation_list_mapping[doi] = references
+    review["references"] = references
 
 # TODO: To be investigated
 # -> Muss ich mir mal genauer anschauen warum die kaputt sind, und ob ich da was machen kann
@@ -90,13 +92,12 @@ print(empty_references)
 
 similarity_edge_list = []
 
-for review_A, references_A in review_citation_list_mapping.items():
-    # TODO: Optimize runtime and prevent duplicates by starting nested for loop at index + 1
-    # https://stackoverflow.com/questions/14538885/how-to-get-the-index-with-the-key-in-a-dictionary
-    for review_B, references_B in review_citation_list_mapping.items():
-        if review_A is review_B:
-            continue
-
+for i, review_A in enumerate(review_meta_mappings):
+    # Optimization: Skip reflection and symmetry (self and items that have already seen each other)
+    for review_B in review_meta_mappings[i+1:]:
+        references_A = review_A["references"]
+        references_B = review_B["references"]
+        
         # Jaccard-Formula
         intersection = len(list(set(references_A).intersection(references_B)))
         union = (len(set(references_A)) + len(set(references_B))) - intersection
@@ -117,38 +118,85 @@ print(f"Mean similarity over all: {sum(edge[2] for edge in similarity_edge_list)
 
 # 3. Visualization
 
-# https://networkx.org/documentation/stable/reference/readwrite/edgelist.html
-
-# hvplot oder so fuer dense graphs
-
 G = nx.Graph()
 
 for n1, n2, w in similarity_edge_list:
     # Show edge only if some similarity exists
-    if w > .001:
-        G.add_edge(n1, n2, weight=w)
+    if w > 0:
+        G.add_edge(n1["question"], n2["question"], weight=w)
 
 degree_dict = dict(G.degree())
 node_sizes = [degree * 2 for node, degree in degree_dict.items()]
 edge_width = [G[u][v]['weight'] * 10 for u, v in G.edges()]
 communities = louvain_communities(G)
+communities1 = louvain_communities(G, weight=None)
+communities2 = louvain_communities(G, weight="weight")
 node_color = [i for i, comm in enumerate(communities) for node in comm]
 
-# nx.draw(G, width=edge_width*100)
+# Analysis of communitites
+
+community_keywords = []
+
+for community in communities:
+    unique_string = ""
+
+    # NOTE: Only works since node label is defined as question above !
+    for question in community:
+        # question = question.removeprefix("What is the effect of")
+        # question = question.removeprefix("What are the effects of")
+        # question = question.removeprefix("How effective is")
+        # question = question.removeprefix("How effective are")
+
+        import nltk
+        from nltk.corpus import stopwords
+
+        nltk.download('stopwords')
+        cachedStopWords = stopwords.words("english")
+        unique_string += ' '.join([word for word in question.split() if word not in cachedStopWords])
+
+        # unique_string += question \
+        #     .replace("?", "") \
+        #     .replace(",", "") \
+        #     .replace("(", "") \
+        #     .replace(")", "") \
+        #     .replace(";", "") \
+        #     .replace(":", "") + " "
+
+
+    d = {}
+    for keyword in unique_string.split(" "):
+        d[keyword] = d.get(keyword, 0) + 1
+
+    ranked = sorted(d.items(), key=lambda x: x[1], reverse=True)
+
+    print(ranked)
+
+    # Wordcloud per community
+    wordcloud = WordCloud(width = 1000, height = 500).generate(unique_string)
+    plt.figure(figsize=(15,8))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    # plt.savefig("your_file_name"+".png", bbox_inches='tight')
+    plt.show()
+    plt.close()
+
+# Was ist falsch mit der wordcloud: carbon viel oefter als organic (auch andere woerter dazwischen)
+# aber beide am groessten dargestellt
+# oder "soil organic" ??
+
+
+
+nx.draw(G, width=edge_width*100)
 # nx.draw(G, node_size=node_sizes)
 # nx.draw(G, width=edge_width, node_size=node_sizes)
 # hvnx.draw(G, with_labels=True)
 
-nx.draw(G, node_color=node_color, width=edge_width, node_size=8, alpha=0.5)
+# nx.draw(G, node_color=node_color, width=edge_width, node_size=8, alpha=0.5)
 
+# plt.show()
+
+# hvnx.draw(G, node_color=node_color, width=edge_width, node_size=8, alpha=0.5)
 plt.show()
-
-# G = nx.petersen_graph()
-
-# spring = hvnx.draw(G, with_labels=True)
-# shell = hvnx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-
-# spring + shell
 
 pass
 
