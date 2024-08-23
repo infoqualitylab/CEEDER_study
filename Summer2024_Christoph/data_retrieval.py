13ae6df7cc6c6893e3beb839101282deae7ae958
unknown <christoph-stade@outlook.de>
2024-08-23 15:01:31 -0500

    Add funder data from crossref to reviews

--- Summer2024_Christoph/data_retrieval.py
+++ Summer2024_Christoph/data_retrieval.py
@@ -2,8 +2,86 @@ import requests
 import json
 from csv import DictReader
 
+
+broken_dois = []
+empty_references = []
+empty_funders = []
+reviews_with_meta_data = []
+
+
+def get_references(doi):
+    response = requests.get(f'https://api.openalex.org/works?filter=doi:{doi}')
+
+    if response.status_code != 200:
+        print(f"Error: Failed to retrieve full text. Status code: {response.status_code}")
+
+    data = response.json()
+
+    if not data.get("results") or data["results"] == []:
+        broken_dois.append(doi)
+        return None 
+
+    references = data["results"][0]["referenced_works"]
+
+    if references == []:
+        empty_references.append(doi)
+        return None
+
+    return references
+
+
+def get_funders(doi):
+    response = requests.get(f'https://api.crossref.org/works/{doi}')
+
+    if response.status_code != 200:
+        print(f"Error: Failed to retrieve full text. Status code: {response.status_code}")
+
+    data = response.json().get("message", None)
+
+    if not data:
+        broken_dois.append(doi)
+        return []
+
+    funders = data.get("funder", None)
+        
+    if not funders:
+        empty_funders.append(doi)
+        return []
+    
+    funders_with_meta_data = []
+    
+    for funder in funders:
+        if not funder.get("DOI", None):
+            funders_with_meta_data.append(funder) # Append to list, handle funders w/o meta in visualization
+            continue
+
+        response = requests.get(f'https://data.crossref.org/fundingdata/funder/{funder["DOI"]}')
+        
+        if response.status_code != 200:
+            print(f"Error: Failed to retrieve full text. Status code: {response.status_code}")
+
+        data = response.json()
+
+        if not data:
+            continue
+
+        funder["region"] = data["region"]
+        funder["fundingBodyType"] = data["fundingBodyType"]
+        funders_with_meta_data.append(funder)
+
+    return funders_with_meta_data
+
+
 def get_review_meta_data():
-    # Structure: [{ "title": "abc", "doi": 123, "question": "abc?", "references": [ 1, 2, 3 ] }, { ... }]
+    # Structure:
+    # [{ 
+    #   "title": "abc", 
+    #   "doi": 123, 
+    #   "question": "abc?",
+    #   "references": [ 1, 2, ... ] }, 
+    #   "funders": [ 1, 2, ... ] }, 
+    #   { ... }
+    # }]
     reviews = [] # TODO: Rename. Not really a mapping. Outermost data structure is a list...
 
     # Extract review DOIs from CEEDER csv dump
@@ -22,41 +100,29 @@ def get_review_meta_data():
                 # Strategy: ignore. Seems to only be a single review
                 pass
 
-    broken_dois = []
-    empty_references = []
-    review_with_references = []
-
     for review in reviews: 
         doi = review["doi"]
-        response = requests.get(f'https://api.openalex.org/works?filter=doi:{doi}')
 
-        if response.status_code != 200:
-            print(f"Error: Failed to retrieve full text. Status code: {response.status_code}")
+        references = get_references(doi) 
+        if not references: continue # Skip on broken DOI (unrecognized by OpenAlex) or empty references
 
-        response_text = json.loads(response.text)
-
-        if not response_text.get("results") or response_text["results"] == []:
-            broken_dois.append(doi)
-            continue 
-
-        references = response_text["results"][0]["referenced_works"]
-
-        if references == []:
-            empty_references.append(doi)
-            continue
-
-        review_with_references.append({
+        funders = get_funders(doi)
+        # Don't skip on broken doi (at crossref API)
+        
+        reviews_with_meta_data.append({
             "title": review.get("title"),
             "doi": review.get("doi"),
             "question": review.get("question"),
-            "references": references
+            "references": references,
+            "funders": funders
         })
 
     # TODO: To be investigated
     # -> Muss ich mir mal genauer anschauen warum die kaputt sind, und ob ich da was machen kann
     # ("Link ist auch nicht immer doi...")
-    print(broken_dois)
-    print(empty_references)
+    # print(broken_dois)
+    # print(empty_references)
 
-    with open("./review_with_references.json", "w") as file:
-        file.write(json.dumps(review_with_references))
+    with open("./reviews_with_meta_data.json", "w") as file:
+        file.write(json.dumps(reviews_with_meta_data))
+    
