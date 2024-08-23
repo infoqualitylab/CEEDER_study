d2dbbd7cf50332f749a2f521c7614ff7259850c8
unknown <christoph-stade@outlook.de>
2024-08-23 15:28:42 -0500

    Add visualization of funders in sunburst pie chart

---
+++ Summer2024_Christoph/visualize_funders.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from json import loads
+
+from data_retrieval import get_review_meta_data
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px 
+
+
+if not Path("./reviews_with_meta_data.json").is_file():
+    get_review_meta_data() 
+
+# Structure:
+# [{ 
+#   "title": "abc", 
+#   "doi": 123, 
+#   "question": "abc?",
+#   "references": [ 1, 2, 3 ] }, 
+#   { ... }
+# }]
+with open("./reviews_with_meta_data.json") as file:
+    reviews_with_meta_data = loads(file.read()) # 340 reviews; 16 with either broken DOIs or empty references
+
+funders_df = pd.DataFrame(
+    [funder for item in reviews_with_meta_data for funder in item['funders'] if funder.get("fundingBodyType", None) and funder.get("region", None)]
+)
+
+df = px.data.tips() 
+  
+# fig = px.sunburst(df, path=['day', 'sex'], values='total_bill') 
+fig = px.sunburst(funders_df, path=['fundingBodyType', 'region', 'name']) 
+fig.show()
