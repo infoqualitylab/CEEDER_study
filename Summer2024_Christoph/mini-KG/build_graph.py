a2ec10b16e324f8a75545cae77e156200e6453ea
unknown <christoph-stade@outlook.de>
2024-09-16 15:12:43 -0500

    Play around with annotating ENVO. Only really makes sense with NER...

--- Summer2024_Christoph/mini-KG/build_graph.py
+++ Summer2024_Christoph/mini-KG/build_graph.py
@@ -1,6 +1,9 @@
-from sentence_template_extraction import regex_slicing
+from csv import DictReader
+import json
 
 import rdflib
+
+from sentence_template_extraction import regex_slicing
 
 
 slices = regex_slicing()
@@ -30,11 +33,77 @@ with open("./mini-KG.ttl", "w", encoding="UTF-8") as file:
     file.writelines(sorted(triples))
 
 
+# TODO: Split research questions
+    # Attachment to originating triple per (nested) reification?
+    # Careful! There exist special cases where the and must not be split
+    #    elevated_atmospheric_carbon_dioxide_concentration_and_temperature
+    # Leave as is for now
+
+# TODO: Skript to append ENVO taxonomy
+#   Mark every concept that wasn't able to be mapped -> manual map to closest or leave out
 
 
 
 
 
+
+# Enrichment
+# with ENVO ontology as taxonomy
+# .. quick and dirty w/o rdflib, just text writes; cleaner would be rdflib... get resources or similar
+
+envo_classes = []
+no_tax = []
+taxonomy_triples = []
+
+with open("C:/Users/chris/Downloads/envo.json", "r", encoding="UTF-8") as file:
+        envo_classes = json.load(file)["graphs"][0]["nodes"]
+
+# with open("C:/Users/chris/Downloads/envo-basic.tsv", "r", encoding="UTF-8") as csvfile:
+#         reader = DictReader(csvfile, delimiter='\t', quotechar='"')
+
+#         for row in reader:
+#             envo_classes.append({
+#                 "ID": row.get("ID"),
+#                 "name": row.get("label")
+#                 # "synonyms": row.get("Review Question") # TODO: Leave out for now... Very valuable, but formatting weird
+#             })
+
+for triple in triples:
+    resources = triple.replace("<< ", "") \
+        .replace(" >>", "") \
+        .replace(" . ", "") \
+        .replace("\n", "") \
+        .replace("<http://ex.com/effects> ", "") \
+        .replace("<http://ex.com/in> ", "") \
+        .split(" ")
+    
+    for resource in resources:
+        name = resource.replace(">", "").split("/")[-1] # Get to string name
+
+        for cla in envo_classes:
+            # if name == cla.get("name"): CSV
+            if name == cla.get("lbl"):
+                envo_prefix = "http://purl.obolibrary.org/obo/ENVO_"
+                # class_id = cla.get("ID") CSV
+                class_id = cla.get("id")
+
+                taxonomy_triples.append(
+                    f"{resource} rdf:type <{envo_prefix}{class_id}>"
+                )
+                # TODO: Here I'll need NER or manual work to identify parent concepts that don't simply name match
+
+                break
+
+        # Python for-else :)
+        else: 
+            no_tax.append(resource)
+            
+
+
+print(f"For {len(taxonomy_triples) / len(triples)}% of resources there was no match in ENVO.")
+
+with open("./mini-KG.ttl", "a", encoding="UTF-8") as file:
+    file.writelines(sorted(taxonomy_triples))
 
 
 
